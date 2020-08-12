aaeb51cc1132014e62c3e13f014993e860252b8c
heliangxx <heliangxx@163.com>
2020-08-12 16:26:07 +0800

    msg update

---
+++ src/main/java/com/pactera/znzmo/sensitivewords/SensitiveWordController.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.pactera.znzmo.sensitivewords;
+
+import java.util.function.Supplier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pactera.znzmo.enums.JsonResultEnum;
+import com.pactera.znzmo.vo.MsgAddParam;
+import com.pactera.znzmo.web.BaseController;
+import com.pactera.znzmo.web.JsonResp;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
+/**
+ * @author heliang
+ *
+ */
+@Api(tags = "敏感词API", value = "敏感词API")
+@RestController
+@RequestMapping(value = "/SensitiveWord")
+public class SensitiveWordController extends BaseController {
+	public static final Logger logger = LoggerFactory.getLogger(SensitiveWordController.class);
+
+	@Autowired
+	TbSensitiveWordsService sensitiveWordService;
+	
+	@ApiOperation(value = "新增敏感词", httpMethod = "POST", notes = "敏感词新增")
+	@RequestMapping(value = "/addSensitiveWord", method = { RequestMethod.POST })
+	public JsonResp addSensitiveWord(
+			@ApiParam(name = "MsgAddParam", value = "系统消息新增参数", required = false) @RequestBody MsgAddParam msgAddParam) {
+		Supplier<String> businessHandler = () -> {
+			try {
+				msgService.addSysMsg(msgAddParam);
+				return JsonResultEnum.ok.getValue();
+			} catch (Exception e) {
+				throwException(e);
+			}
+			return JsonResultEnum.fail.getValue();
+		};
+		return handleRequest(businessHandler);
+	}
+
+}
